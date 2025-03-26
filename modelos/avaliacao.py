61451a3157d7358fcf9e17ef4a41e2ebf1c3922c
Isaac Davi <izaacddavid98@gmail.com>
2025-03-26 09:34:49 -0300

    Chore

--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -2,7 +2,7 @@ class Avaliacao:
     def __init__(self, cliente, nota):
         self._cliente = cliente
         self._nota = nota
-        self._avaliacao = []  # Initialize _avaliacao as an empty list
+        self._avaliacao = [] 
     
     def __str__(self):
         return f'{self._cliente} | {self._nota}'
